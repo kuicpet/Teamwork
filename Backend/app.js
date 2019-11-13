0a782c1ad470aea95311572199d4815c2deb9203
Kingsley <kingsleyumujeyan@gmail.com>
2019-11-13 11:14:57 +0100

    Api endpoints

--- Backend/app.js
+++ Backend/app.js
@@ -1,33 +1,58 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
 
-app.get("/", (request, response) => {
+app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.post("/auth/create-user",(request, response, next) =>{
+    const data = [
+        {
+            'firstName': String,
+            'lastName': String,
+            'email': String,
+            'password': String,
+            'gender': String,
+            'jobRole': String,
+            'department': String,
+            'address': String,
+        },
+    ]
+    response.status(201).send({
+        'status': 'success',
+        'data' : {
+            'message': 'User account successfully created',
+            'token': 'String',
+            'userId': 'Interger'
+        }
+    });
+    next();
+});
+  app.post("/auth/signin",(resquest, response, next) =>{
+      
+  })
+
+app.get("/", (request, response, next) => {
     response.status(200).send({
         message: "Your request was a success!"
     });
+    next();
 });
 
-app.get("/test", (request, response) => {
+app.get("/test", (request, response, next) => {
     response.status(500).send({ "message": "This is an error response" });
-});
-/*
-app.get('/',(req, res, next)=>{
-    res.status(200).send({message:'Your request was a Success!'});
     next();
 });
-app.get('/test',(req, res, next) =>{
-    res.status(500).send({message:'This is an Error Message'});
-    next();
-});
-app.use((req, res, next)=>{
-    res.json({message :'Your request was successful!'});
-    next();
-});
-app.use((req, res,next)=>{
-    console.log('Response sent successfully!');
-});*/
+
+
+
 
 
 module.exports = app;
